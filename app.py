7d23eeea22c8daf28456729c3e8bf02f05972893
eugenia <eugeniarcvm@gmail.com>
2026-09-16 14:26:55 -0300

    Configura estrutura inicial da API Flask

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
 
+CORS(app)
+
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///clinica.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 
@@ -11,7 +15,7 @@ db = SQLAlchemy(app)
 @app.route("/")
 def inicio():
     return jsonify({
-        "mensagem": "API da Clínica Veterinária UNIESP"
+        "mensagem": "API da Clínica Veterinária UNIESP funcionando!"
     })
 
 
